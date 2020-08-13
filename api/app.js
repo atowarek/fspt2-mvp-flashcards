54fadc51c4243db09552fae5ba9f94de6d85d556
Anna Towarek <atowarek@gmail.com>
2020-08-13 19:40:41 +0200

    ci: deployment

--- api/app.js
+++ api/app.js
@@ -2,11 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
+
+const clientPath = path.join(__dirname, '../', 'client', 'build')
 
 const apiGamesRoutes = require('./routes/games')
 const apiTriviaCategoryRoutes = require('./routes/categories')
 
 const app = express()
+app.use(express.static(clientPath))
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -16,9 +20,16 @@ app.use('/api/categories', apiTriviaCategoryRoutes)
 
 app.get('/api', (req, res) => {
   res.send({
-    message: 'hola',
+    message: 'hello',
   })
 })
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(clientPath, 'index.html'))
+})
+
+//app.use(express.static('./client/public/img'))
+app.use(express.static(clientPath))
 
 app.listen(process.env.PORT, () => {
   console.log(`Starting server in PORT ${process.env.PORT}`)
