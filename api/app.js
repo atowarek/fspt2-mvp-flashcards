0f5d642eb82a40398eb91fda2ee3de3f2b0fb74f
Anna Towarek <atowarek@gmail.com>
2020-08-13 18:56:49 +0200

    chore: fix port

--- api/app.js
+++ api/app.js
@@ -20,6 +20,35 @@ app.get('/api', (req, res) => {
   })
 })
 
+app.listen(process.env.PORT, () => {
+  console.log(`Starting server in PORT ${process.env.PORT}`)
+})
+
+/*
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const apiGamesRoutes = require('./routes/games')
+const apiTriviaCategoryRoutes = require('./routes/categories')
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/games', apiGamesRoutes)
+app.use('/api/categories', apiTriviaCategoryRoutes)
+
+app.get('/api', (req, res) => {
+  res.send({
+    message: 'hola',
+  })
+})
+
 app.listen(process.env.API_PORT, () => {
   console.log(`Starting server in PORT ${process.env.API_PORT}`)
 })
+
+*/
